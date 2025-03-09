d2703084db6c125d1b9730c65cebfab00bd864ca
t2tc <61874231+t2tc@users.noreply.github.com>
2025-03-09 22:34:35 +0800

    move frontend folder.

--- frontend/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import UnoCSS from 'unocss/vite'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), UnoCSS()],
-})
